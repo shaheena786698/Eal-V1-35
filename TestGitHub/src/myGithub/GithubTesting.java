13ce7b8eaa93a71f7707eb25fed485ca823211e1
Shaheena Rahman <Shaheena Rahman@DESKTOP-2JROAG6.lan>
2024-05-25 16:32:08 -0400

    This Is Master Branch. Please download this master branch.

---
+++ TestGitHub/src/myGithub/GithubTesting.java
@@ -0,0 +1,14 @@
+package myGithub;
+
+public class GithubTesting {
+
+	public static void main(String[] args) {
+	 String fname= "Mamtaz";
+	 String lname= "Rahman";
+	 String fullname= fname+lname;
+		 
+		 System.out.println("my name is: " +fullname);
+
+	}
+
+}
